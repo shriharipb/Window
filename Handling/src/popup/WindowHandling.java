96c42f4fd38eea899cfbb0d0ce284a1654743d20
shriharipb <123650545+shriharipb@users.noreply.github.com>
2023-01-30 09:37:41 +0530

    Update WindowHandling.java

--- Handling/src/popup/WindowHandling.java
+++ Handling/src/popup/WindowHandling.java
@@ -5,6 +5,8 @@ public class WindowHandling {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Selenium Window Handling");
+
+		System.out.println("Window Popup");
 	}
 	
 	
